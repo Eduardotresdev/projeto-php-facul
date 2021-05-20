66f1c855c1c8ba416d2c6af0443288a0df35e83c
Eduardo A.Tres de Souza <devtres19@gmail.com>
2021-05-19 21:40:06 -0300

    Aula 19-05-2021 - Cadastro

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', '127.0.0.1');
 define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'login');
+define('SENHA', 'root');
+define('DB', 'projeto_facul');
 
 $connection = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
